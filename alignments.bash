42132eb3e42ba2c374adcf400bd781bd4e69d031
N0toriou5 <49403790+N0toriou5@users.noreply.github.com>
2022-11-19 16:17:17 +0100

    Add files via upload

---
+++ alignments.bash
@@ -0,0 +1,156 @@
+# TEPA Orazio
+# concatenate together separate fastq files from the same sample
+cd /mnt/f/Projects/Orazio/TEPA/SAL9125/SAL9125A1
+cat DIPG007_CON_1_S1_L001_R1_001.fastq.gz DIPG007_CON_1_S1_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_1_S1_L001_R1_001.fastq.gz
+cat DIPG007_CON_1_S1_L001_R2_001.fastq.gz DIPG007_CON_1_S1_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_1_S1_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A1/
+cd SAL9125A10
+cat DIPG010_TEPA_1_S18_L001_R1_001.fastq.gz DIPG010_TEPA_1_S18_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_1_S18_L001_R1_001.fastq.gz
+cat DIPG010_TEPA_1_S18_L001_R2_001.fastq.gz DIPG010_TEPA_1_S18_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_1_S18_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A10
+cd SAL9125A11
+cat DIPG010_TEPA_2_S19_L001_R1_001.fastq.gz DIPG010_TEPA_2_S19_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_2_S19_L001_R1_001.fastq.gz
+cat DIPG010_TEPA_2_S19_L001_R2_001.fastq.gz DIPG010_TEPA_2_S19_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_2_S19_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A11
+cd SAL9125A12
+cat DIPG010_TEPA_3_S9_L001_R1_001.fastq.gz DIPG010_TEPA_3_S9_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_3_S9_L001_R1_001.fastq.gz
+cat DIPG010_TEPA_3_S9_L001_R2_001.fastq.gz DIPG010_TEPA_3_S9_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_TEPA_3_S9_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A12
+cd SAL9125A13
+cat MB231_CON_1_S17_L001_R1_001.fastq.gz MB231_CON_1_S17_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_1_S17_L001_R1_001.fastq.gz
+cat MB231_CON_1_S17_L001_R2_001.fastq.gz MB231_CON_1_S17_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_1_S17_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A13
+cd SAL9125A14
+cat MB231_CON_2_S7_L001_R1_001.fastq.gz MB231_CON_2_S7_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_2_S7_L001_R1_001.fastq.gz
+cat MB231_CON_2_S7_L001_R2_001.fastq.gz MB231_CON_2_S7_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_2_S7_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A14
+cd SAL9125A15
+cat MB231_CON_3_S13_L001_R1_001.fastq.gz MB231_CON_3_S13_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_3_S13_L001_R1_001.fastq.gz
+cat MB231_CON_3_S13_L001_R2_001.fastq.gz MB231_CON_3_S13_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_CON_3_S13_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A15
+cd SAL9125A16
+cat MB231_TEPA8_1_S2_L001_R1_001.fastq.gz MB231_TEPA8_1_S2_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_1_S2_L001_R1_001.fastq.gz
+cat MB231_TEPA8_1_S2_L001_R2_001.fastq.gz MB231_TEPA8_1_S2_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_1_S2_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A16
+cd SAL9125A17
+cat MB231_TEPA8_2_S10_L001_R1_001.fastq.gz MB231_TEPA8_2_S10_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_2_S10_L001_R1_001.fastq.gz
+cat MB231_TEPA8_2_S10_L001_R2_001.fastq.gz MB231_TEPA8_2_S10_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_2_S10_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A17
+cd SAL9125A18
+cat MB231_TEPA8_3_S14_L001_R1_001.fastq.gz MB231_TEPA8_3_S14_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_3_S14_L001_R1_001.fastq.gz
+cat MB231_TEPA8_3_S14_L001_R2_001.fastq.gz MB231_TEPA8_3_S14_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA8_3_S14_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A18
+cd SAL9125A19
+cat MB231_TEPA24_1_S8_L001_R1_001.fastq.gz MB231_TEPA24_1_S8_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_1_S8_L001_R1_001.fastq.gz
+cat MB231_TEPA24_1_S8_L001_R2_001.fastq.gz MB231_TEPA24_1_S8_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_1_S8_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A19
+cd SAL9125A2
+cat DIPG007_CON_2_S20_L001_R1_001.fastq.gz DIPG007_CON_2_S20_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_2_S20_L001_R1_001.fastq.gz
+cat DIPG007_CON_2_S20_L001_R2_001.fastq.gz DIPG007_CON_2_S20_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_2_S20_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A2
+cd SAL9125A20
+cat MB231_TEPA24_2_S4_L001_R1_001.fastq.gz MB231_TEPA24_2_S4_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_2_S4_L001_R1_001.fastq.gz
+cat MB231_TEPA24_2_S4_L001_R2_001.fastq.gz MB231_TEPA24_2_S4_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_2_S4_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A20
+cd SAL9125A21
+cat MB231_TEPA24_3_S11_L001_R1_001.fastq.gz MB231_TEPA24_3_S11_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_3_S11_L001_R1_001.fastq.gz
+cat MB231_TEPA24_3_S11_L001_R2_001.fastq.gz MB231_TEPA24_3_S11_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/MB231_TEPA24_3_S11_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A21
+cd SAL9125A3
+cat DIPG007_CON_3_S6_L001_R1_001.fastq.gz DIPG007_CON_3_S6_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_3_S6_L001_R1_001.fastq.gz
+cat DIPG007_CON_3_S6_L001_R2_001.fastq.gz DIPG007_CON_3_S6_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_CON_3_S6_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A3
+cd SAL9125A4
+cat DIPG007_TEPA_1_S16_L001_R1_001.fastq.gz DIPG007_TEPA_1_S16_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_1_S16_L001_R1_001.fastq.gz
+cat DIPG007_TEPA_1_S16_L001_R2_001.fastq.gz DIPG007_TEPA_1_S16_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_1_S16_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A4
+cd SAL9125A5
+cat DIPG007_TEPA_2_S5_L001_R1_001.fastq.gz DIPG007_TEPA_2_S5_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_2_S5_L001_R1_001.fastq.gz
+cat DIPG007_TEPA_2_S5_L001_R2_001.fastq.gz DIPG007_TEPA_2_S5_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_2_S5_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A5
+cd SAL9125A6
+cat DIPG007_TEPA_3_S15_L001_R1_001.fastq.gz DIPG007_TEPA_3_S15_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_3_S15_L001_R1_001.fastq.gz
+cat DIPG007_TEPA_3_S15_L001_R2_001.fastq.gz DIPG007_TEPA_3_S15_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG007_TEPA_3_S15_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A6
+cd SAL9125A7
+cat DIPG010_CON_1_S3_L001_R1_001.fastq.gz DIPG010_CON_1_S3_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_1_S3_L001_R1_001.fastq.gz
+cat DIPG010_CON_1_S3_L001_R2_001.fastq.gz DIPG010_CON_1_S3_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_1_S3_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A7
+cd SAL9125A8
+cat DIPG010_CON_2_S21_L001_R1_001.fastq.gz DIPG010_CON_2_S21_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_2_S21_L001_R1_001.fastq.gz
+cat DIPG010_CON_2_S21_L001_R2_001.fastq.gz DIPG010_CON_2_S21_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_2_S21_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A8
+cd SAL9125A9
+cat DIPG010_CON_3_S12_L001_R1_001.fastq.gz DIPG010_CON_3_S12_L002_R1_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_3_S12_L001_R1_001.fastq.gz
+cat DIPG010_CON_3_S12_L001_R2_001.fastq.gz DIPG010_CON_3_S12_L002_R2_001.fastq.gz > /mnt/f/Projects/Orazio/TEPA/DIPG010_CON_3_S12_L001_R2_001.fastq.gz
+cd ..
+rm -r SAL9125A9
+
+
+# fastqc
+mkdir fastqc
+for fname in *_001.fastq.gz
+do
+root=`basename $fname _001.fastq.gz`
+echo "Doing $root"
+fastqc ${root}_001.fastq.gz -o fastqc/ -t 6
+done
+
+# adapter trimming (not required in this case)
+mkdir trimmed
+for fname in *_R1_001.fastq.gz
+do
+root=`basename $fname _R1_001.fastq.gz`
+echo "Doing $root"
+fastp -i ${root}_R1_001.fastq.gz -I ${root}_R2_001.fastq.gz -o trimmed/${root}_R1_trimmed.fastq.gz -O trimmed/${root}_R2_trimmed.fastq.gz
+done
+
+# align with hisat2, point to hisat2 index
+mkdir hisat_bams
+index=/mnt/f/genomes/Human/hisat_index/hg38
+# start alignment
+for fname in *_R1_001.fastq.gz
+do
+cd /mnt/f/Projects/Orazio/TEPA/
+root=`basename $fname _R1_001.fastq.gz`
+echo "Doing $root"
+hisat2 -x $index \
+-p 6 \
+-t \
+-1 ${root}_R1_001.fastq.gz \
+-2 ${root}_R2_001.fastq.gz \
+| samtools view -bS - > hisat_bams/${root}.bam
+# Sort and index BAMs
+cd /mnt/f/Projects/Orazio/TEPA/hisat_bams
+samtools sort -m 2G -@ 6 -O BAM -o ${root}.sorted.bam ${root}.bam 
+samtools index ${root}.sorted.bam
+rm ${root}.bam
+done
+
+
+### Convert to counts with the subread package
+cd /mnt/f/Projects/Orazio/TEPA/hisat_bams
+mkdir rawcounts
+# humand gtf location: /mnt/f/genomes/Human/GTF
+gtf=/mnt/f/genomes/Human/GTF/Homo_sapiens.GRCh38.103.gtf
+featureCounts -T 7 -p -s 2 -t exon -g gene_name -a $gtf -o rawcounts/featureCounts.txt *sorted.bam
